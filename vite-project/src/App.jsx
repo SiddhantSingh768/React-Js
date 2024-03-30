7dd38954d639ffe94eeb51e56d7df9712c56f531
siddhant <killersiddhant768@gmail.com>
2024-03-30 22:03:32 +0530

    Hooks in React

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 function App() {
+  let [Counter, SetCounter] = useState(15);
+  const addValue = () => {
+    Counter++;
+    console.log("clicked", Counter);
+    SetCounter(Counter);
+  };
+  const subValue = () => {
+    Counter--;
+    console.log("clicked", Counter);
+    SetCounter(Counter);
+  };
   return (
     <>
-    <h1>Siddhant </h1>
+      <h1>React</h1>
+      <h2>Counter value : {Counter}</h2>
+      <button onClick={addValue}>add value</button>
+      <br />
+      <button onClick={subValue}>Remove value</button>
     </>
   );
 }
